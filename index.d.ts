f3863f594aa8b1ff8db3431884b6991130efa6c5
xg4 <xingor4@gmail.com>
2022-07-05 11:52:37 +0800

    feat: add core

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export declare function getBranchName(): string
+export declare function getLastTag(): string
+export declare function getLastCommit(): string
+export declare function getVersion(): string
