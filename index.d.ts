5ba0470568111ec21a2ae8e2174485ee797b1a5f
xg4 <xingor4@gmail.com>
2022-07-05 14:05:00 +0800

    feat: cjs fallback

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function getBranchName(): string
-export declare function getLastTag(): string
-export declare function getLastCommit(): string
-export declare function getVersion(): string
